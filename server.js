753990026143219be3fa27e315f264965340ed15
Karthik Arumugam <chivalrykarthik@gmail.com>
2021-01-03 22:20:44 +0530

    Added pm2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.json({ msg: "Welcome to jenkins" });
+    return res.json({ msg: "Welcome to jenkins build app" });
 })
 
 app.listen(3000)
